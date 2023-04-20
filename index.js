f230a5de2264179199773de90b2fb762c005f490
exactshubham <exactshubham@gmail.com>
2023-04-20 16:13:13 +0530

    Task Completed

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const multer = require('multer');
 const xlsx = require('xlsx');
 app.use(express.json());
+
+// Import the route and initialize multer and xlsx middleware
 const route = require('./src/routes/route')(multer({ storage: multer.memoryStorage() }), xlsx);
 
+// Load environment variables from config file
 require("dotenv").config({path:"./config.env"});
 
+// Disable strictQuery to allow querying on non-existent fields
 mongoose.set('strictQuery', false);
+
+// Connect to the database
 mongoose
   .connect('mongodb+srv://ShubhamChaturvedi:9555047172@mongodbwithshubham.z3dowao.mongodb.net/insuredminedata(csv)', {
     useNewUrlParser: true,
@@ -23,10 +27,9 @@ mongoose
   });
 
 // Use middleware only for the routes that need it
-
 app.use('/', route);
 
+// Start the server
 app.listen(process.env.PORT || 3001, function () {
   console.log('Express app running on PORT ' + (process.env.PORT || 3001));
-});
-
+}); 
